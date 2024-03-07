11cd4182833490d3fbb7702e4c17a80a8e998240
EDUEIRDO <cadusilvateno@gmail.com>
2024-03-07 17:13:18 -0300

    bugfix

--- projeto_abrigo_amigo/app_abrigo_amigo/views.py
+++ projeto_abrigo_amigo/app_abrigo_amigo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def banco(request):
+    return render(request, 'admin/db.html')
--- projeto_abrigo_amigo/projeto_abrigo_amigo/urls.py
+++ projeto_abrigo_amigo/projeto_abrigo_amigo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from app_abrigo_amigo import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.banco, name='banco'),
 ]
